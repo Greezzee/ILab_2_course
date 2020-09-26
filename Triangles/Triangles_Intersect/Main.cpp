c31484ffea930e19bfd41a8e4ed4eb90544ccc5f
Евгений Зорин <zorin.eugene@gmail.com>
2020-09-26 22:58:04 +0600

    Fix: now have console input

--- Triangles/Triangles_Intersect/Main.cpp
+++ Triangles/Triangles_Intersect/Main.cpp
@@ -10,24 +10,20 @@ struct CollidableTriangle {
 
 int main()
 {
-	std::ifstream file;
-	file.open("10000.1.txt");
-
 	size_t triangle_count = 0;
-	file >> triangle_count;
+	std::cin >> triangle_count;
 
 	Space space;
 	for (size_t i = 0; i < triangle_count; i++) {
 		Vector3F a, b, c;
-		file >> a.x >> a.y >> a.z;
-		file >> b.x >> b.y >> b.z;
-		file >> c.x >> c.y >> c.z;
+		std::cin >> a.x >> a.y >> a.z;
+		std::cin >> b.x >> b.y >> b.z;
+		std::cin >> c.x >> c.y >> c.z;
 		std::vector<Vector3F> triangle_points = {a, b, c};
 		PolygonCollider collider;
 		collider.Init(triangle_points);
 		space.AddTriangle(collider);
 	}
-	file.close();
 	space.CreateSpace();
 	space.FindAndPrintCollidingTriangles();
 
